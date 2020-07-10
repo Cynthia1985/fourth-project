04011e69f540f301bd0db1e456f65f32a73429de
CynthiaEspejo <cynthiaespejo14@hotmail.com>
2020-07-10 14:23:22 -0700

    api donone

---
+++ list_app/list_app_api/config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root 'lists#index'
+  resources :lists do 
+    resources :list_items, only: [:create, :show]
+  end
+  resources :list_items
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
